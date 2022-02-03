462ed43b4f27cfa1bb1fb4b7d85e201d51b4b492
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-02-03 14:55:25 +0200

    finish key events

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # The-Odin-Project-Calculator
+
+# Fix Later
+-- Can't use more than 2 numbers
+-- I want to prevent the user from deleting lastOperation Result
+-- Stop the user from printing ---- or +++ 
--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let previousNum = "";
 let sign = "";
 let numbers = [];
 let lastOperation = null;
-
 /********************************************************************************************************/
 /*------------------- Helper Functions -----------------*/
 function reset() {
@@ -24,65 +23,37 @@ function reset() {
   previousNum = "";
 
   // Reset DOM Elements
-  resultElement.textContent = "";
+  resultElement.textContent = "0";
   detailsElement.innerHTML = "&nbsp;";
 }
 
 function deleteNum() {
-  // Convert The String Previous Num To Array - Remove Last Number - Convert It Back To String - Store The Result In Previous Num
+  // To Prevent The User From Deleting The Result Value
+
+   // Convert The String Previous Num To Array - Remove Last Number - Convert It Back To String - Store The Result In Previous Num
   previousNum = previousNum.split("").splice(0, previousNum.length - 1).join("");
 
   // Update The UI
+
   resultElement.textContent = previousNum;
+  
 }
-/********************************************************************************************************/
 
-numbersContainer.addEventListener("click", function (e) {
-  // Check If The Element That Was Clicked Is A Button Or Not
-  if(e.target.classList.contains("btn-number")) {
-    // Extract The Number Or The Sign
-    currNumOrSign = e.target.textContent;
-    
-
-    if(currNumOrSign !== "+" && currNumOrSign !== "=" && currNumOrSign !== "−" && currNumOrSign !== "×" && currNumOrSign !== "÷" ) {
-    // If Number Was Clicked Then Change Previous Num To Be The Clicked Number & Update The UI
-      previousNum += currNumOrSign;
-      resultElement.textContent = previousNum;
-    }
-
-    if(currNumOrSign === "+" || currNumOrSign === "−" || currNumOrSign === "×" || currNumOrSign === "÷" ) {
-      // Extract The Sign
-      sign = currNumOrSign;
-
-      // If Last Operation Has Value , Then Clear Numbers Array & Assign The Last Operation To Be The First Element In The Array
-      if(lastOperation !== null) {
-        numbers = [];
-        numbers[0] = lastOperation;
-
-        // Update The UI
-        detailsElement.textContent = `${lastOperation} ${sign} `;
-      }
-
-      // If LastOperation Is Not Defined ( Not Operation Done Yet) Then Push Previous Num TO Numbers Array & Update The UI
-      if (lastOperation === null) {
-        numbers.push(previousNum);
-        detailsElement.textContent += `${previousNum} ${sign} `
-      }
- 
-      // Removing Previous As We Already Stored It Into The Array In Line 71 - To Prevent The New Num From Joining The Old Num
-      previousNum = "";
-    }
-
-    if (currNumOrSign === "=") {
-      // Update Numbers Array Again To Store The Previous Number Upon Clicking = 
-      numbers.push(previousNum);
-      lastOperation = calculate(sign);
-
-      // Update The UI
-      detailsElement.textContent += `${previousNum} =`
-    }
+function convertInputToSymbols(input) {
+  if(input === '-') {
+    input = '−'
   }
-});
+  if(input === '*') {
+    input = '×'
+  }
+  if(input === '/') {
+    input = '÷'
+  }
+  if(input === 'Enter') {
+    input = '='
+  }
+  return input;
+}
 
 function calculate(sign) {
   const [firstNum, secondNum] = numbers;
@@ -102,6 +73,67 @@ function calculate(sign) {
   resultElement.textContent = result;
   return result;
 }
+
+function handleOperation(numberOrSign) {
+  // This Is for Key Press Event
+   numberOrSign = convertInputToSymbols(numberOrSign);
+
+  if(!isNaN(numberOrSign)) {
+    // If Number Was Clicked Then Change Previous Num To Be The Clicked Number & Update The UI
+      previousNum += numberOrSign;
+      resultElement.textContent = previousNum;
+    }
+
+    if(numberOrSign === "+" || numberOrSign === "−" || numberOrSign === "×" || numberOrSign === "÷" ) {
+
+      // Extract The Sign
+      sign = numberOrSign;
+
+      // If Last Operation Has Value , Then Clear Numbers Array & Assign The Last Operation To Be The First Element In The Array
+      if(lastOperation !== null) {
+        numbers = [];
+        numbers[0] = lastOperation;
+
+        // Update The UI
+        detailsElement.textContent = `${lastOperation} ${sign} `;
+      }
+
+      // If LastOperation Is Not Defined ( Not Operation Done Yet) Then Push Previous Num TO Numbers Array & Update The UI
+      if (lastOperation === null) {
+        numbers.push(previousNum);
+        detailsElement.textContent += `${previousNum} ${sign} `
+      }
+ 
+      // Removing Previous As We Already Stored It Into The Array - To Prevent The New Num From Joining The Old Num
+      previousNum = "";
+    }
+
+    if (numberOrSign === "=") {
+      // Update Numbers Array Again To Store The Previous Number Upon Clicking = 
+      numbers.push(previousNum);
+      lastOperation = calculate(sign);
+ 
+      // Only Update The UI If We Have A Result 
+      if(lastOperation) {
+         // Update The UI
+         detailsElement.textContent += `${previousNum} =`
+      }
+     
+    }
+}
+/********************************************************************************************************/
+/*------------------- DOM Events -----------------*/
+numbersContainer.addEventListener("click", function (e) {
+  // Check If The Element That Was Clicked Is A Button Or Not
+  if(e.target.classList.contains("btn-number")) {
+    // Extract The Number Or The Sign
+    currNumOrSign = e.target.textContent;
+    
+    handleOperation(currNumOrSign);
+  }
+});
+
+
 clearBtnElement.addEventListener("click", function () {
   reset();
 });
@@ -109,6 +141,23 @@ clearBtnElement.addEventListener("click", function () {
 deleteBtnElement.addEventListener("click", function () {
   deleteNum();
 });
+
+
+window.addEventListener('keydown', function(e) {
+  handleOperation(e.key);
+
+  if(e.key === 'Backspace' || e.key === 'Delete') {
+    deleteNum();
+  }
+
+  if(e.key === 'Escape') {
+    reset();
+  }
+
+ 
+})
+
+
 
 
 
